00401752b7134c375fa360ddec84f273c59bc2cf
= <mrupnarine@gmail.com>
2019-09-11 20:48:37 -0400

    Completed hide and seek assignment

--- HideAndSeek/src/app/app.component.ts
+++ HideAndSeek/src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HideAndSeek';
+  isHidden = false;
+  logClickArray = [];
+
+  hideParagraph()
+  {
+    this.logClicks();
+    if (this.isHidden === false)  
+    {
+      this.isHidden = true;      
+    }
+    else
+    {
+      this.isHidden = false;
+    }
+  }
+
+  logClicks()
+  {
+    this.logClickArray.push(this.logClickArray.length + 1);
+  }
+
+  fifthRowStyle(arrayIndex)
+  {
+    if (arrayIndex >= 5)
+    {
+      return "blue";
+    }
+  }
+
+  fifthRowClass(arrayIndex)
+  {
+    if (arrayIndex >= 5)
+    {
+      return ".white";
+    }
+  }
 }
